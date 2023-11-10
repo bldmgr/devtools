a0e2dd067280619bca6f4561f6c2636556433f01
Mike Montpetit <mimontpe@cisco.com>
2023-11-10 12:59:19 -0500

    json

---
+++ json.go
@@ -0,0 +1,12 @@
+package devtools
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+func PrettyPrint(b []byte) ([]byte, error) {
+	var out bytes.Buffer
+	err := json.Indent(&out, b, "", "  ")
+	return out.Bytes(), err
+}
